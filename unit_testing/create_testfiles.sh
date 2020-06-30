d4780f01d9def391e6e0babe0fe6903fc8c73a22
2b <tt@log49.com>
2020-06-30 12:58:46 +0200

    prepare for merge

--- unit_testing/create_testfiles.sh
+++ unit_testing/create_testfiles.sh
@@ -1,6 +1,6 @@
-# echo "Source directory: "
-# read src
-# cp $src ./tests
+echo "Source directory: "
+read src
+cp $src ./tests
 find . ! -name "*.cpp" -exec rm {} \;
 # cd tests
 # find . -name "*" -execdir rename -d 's/^/tst_/' *\;
